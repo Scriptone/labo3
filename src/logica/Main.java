37a31bb8939d5260ba4521a2e73344c76e400805
Scriptone <arne.haers@student.odisee.be>
2023-10-12 09:03:10 +0200

    SubsetSum added

--- src/logica/Main.java
+++
@@ -1,9 +0,0 @@
-package logica;
-
-public class Main {
-
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hola Mundo");
-    }
-}
---
+++ src/logica/SubsetSum.java
@@ -0,0 +1,17 @@
+package logica;
+
+public class SubsetSum {
+
+    /*
+     * Gegeven een getal M en een lijst van positieve getallen. Schrijf een
+     * programma waarbij je aanduidt welke getallen uit die lijst exact sommeren tot
+     * dat ene getal M. Elk getal mag maximaal 1 keer gebruikt worden in de som. Er
+     * kunnen meerdere oplossingen zijn maar het is ok als je programma 1 oplossing
+     * weergeeft. Het kan ook zijn dat er geen oplossing is.
+     */
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hola Mundo");
+    }
+}
